f1ae050759d951908192c37c4fb2388b75b7f0d3
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2024-03-22 23:13:32 +0530

    Add files via upload

---
+++ Hashset.java
@@ -0,0 +1,14 @@
+
+public class Hashset<T> {
+
+    public void add(int num) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    public boolean contains(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+}
